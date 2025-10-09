d5cb939c01bb407b986105968ca9075cfb4a362c
Sumit <Sumitgiri112341@outlook.com>
2025-10-09 22:28:42 +0530

    some changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import UserContext from "./context/userContext.jsx";
+import UserContext from "./context/UserContext.jsx";
 import React from "react";
 
 createRoot(document.getElementById("root")).render(
